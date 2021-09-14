a47726e387b105e6bd1bd088e9351b61ca9b94aa
Rakesh R Menon <rockmenon@icloud.com>
2021-09-14 15:46:33 -0400

    Update setup.sh

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,4 +1,4 @@
 source env/bin/activate
-export PET_ELECTRA_ROOT=`pwd`
-export PYTHONPATH=$PET_ELECTRA_ROOT:$PYTHONPATH
+export ADAPET_ROOT=`pwd`
+export PYTHONPATH=$ADAPET_ROOT:$PYTHONPATH
 export PYTHON_EXEC=python
